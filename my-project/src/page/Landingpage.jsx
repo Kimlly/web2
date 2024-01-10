b2a8af6d6392e3e165b7d6758b9bffd70788f38d
Rupp-ITE-G8-A Y KIMLY <139097271+Kimlly@users.noreply.github.com>
2024-01-10 22:03:32 +0700

    update

--- my-project/src/page/Landingpage.jsx
+++ my-project/src/page/Landingpage.jsx
@@ -10,7 +10,7 @@ function Landingpage() {
                     <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                         <div className="mr-auto place-self-center lg:col-span-7">
                             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Welcome to MΣЯΛKI</h1>
-                            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Join us on this journey as we celebrate the beauty of art and empower artists to share their passion with the world. Meraki is where extraordinary talent finds its true audience, and where creativity thrives.</p>
+                            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Join Us on this journey as we celebrate the beauty of art and empower artists to share their passion with the world. Meraki is where extraordinary talent finds its true audience, and where creativity thrives.</p>
                             <div className="flex items-center md:order-2 space-x-1 md:space-x-2 rtl:space-x-reverse">
                                 <Link to='/login'
                                     className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Login</Link>
